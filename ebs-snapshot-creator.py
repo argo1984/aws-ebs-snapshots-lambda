2b1ebf12ad1f4b96930349eaa0e3e67651c9790f
Falko König <falko@teaminternet.com>
2017-04-13 16:00:20 +0200

    Auto Determine region + tagging instance name

--- ebs-snapshot-creator.py
+++ ebs-snapshot-creator.py
@@ -1,12 +1,22 @@
 import boto3
 import collections
-import datetime
-
-region = 'us-west-2'    # region we're running in (should be changed to be auto-determined 
+from datetime import datetime
 
 ec = boto3.client('ec2')
 
 def lambda_handler(event, context):
+    regions = [context.invoked_function_arn.split(':')[3]]
+    if 'regions' in event:
+        regions = event['regions']
+
+    print("AWS snapshot backups stated at %s...\n" % datetime.now())
+    for region in regions:
+        print("Region: %s" % region)
+        create_region_snapshots(region)
+    print("\nAWS snapshot backups completed at %s\n" % datetime.now())
+
+# create snapshot for region
+def create_region_snapshots(region):
     reservations = ec.describe_instances(
         Filters=[
             { 'Name': 'tag:Backup', 'Values': ['Yes'] },
@@ -48,15 +58,22 @@ def lambda_handler(event, context):
                     continue
                 else:
                     instance_name = tag['Value']
-            
+
             description = '%s - %s (%s)' % ( instance_name, vol_id, dev_name )
 
             # trigger snapshot
             snap = ec.create_snapshot(
-                VolumeId=vol_id, 
+                VolumeId=vol_id,
                 Description=description
                 )
-            
+
+            ec.create_tags(
+                Resources=[snap['SnapshotId']],
+                Tags=[{ 'Key': 'ParentInstance', 'Value': instance['InstanceId'] },
+                      { 'Key': 'MountPoint', 'Value': dev['DeviceName'] },
+                      { 'Key': 'Name', 'Value': instance_name + '_backup' }]
+            )
+
             if (snap):
                 print "\t\tSnapshot %s created in %s of [%s]" % ( snap['SnapshotId'], region, description )
             to_tag[retention_days].append(snap['SnapshotId'])
